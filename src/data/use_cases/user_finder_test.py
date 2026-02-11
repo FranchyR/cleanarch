5c39b16af9698a553a05fb25be73e85ed7edcf7a
FranchyR <franchyrech@gmail.com>
2026-02-11 16:25:39 -0300

    feat: Iimplementing use_cases

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,1 +1,21 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_finder import UserFinder
 
+
+def test_find():
+    first_name = 'meunome'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    print(response)
+    print()
+    print(repo.select_user_attributes)
+
+    assert repo.select_user_attributes["first_name"] == first_name
+
+    assert response["type"] == "Users"
+    assert response["count"] == len(response["attributes"])
+    assert response["type"] != ['']
